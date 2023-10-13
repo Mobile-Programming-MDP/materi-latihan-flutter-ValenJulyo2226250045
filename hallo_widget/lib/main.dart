f74e8f6acb3d021ccd8619d765939c9ce382cd44
Ibnu-Choldun <valenjulyoarmandodavincylin_2226250045@mhs.mdp.ac.id>
2023-10-13 18:40:47 +0700

    2rd commit

--- hallo_widget/lib/main.dart
+++ hallo_widget/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Hello Widget",
+        title: "Hello Widgt",
         theme: ThemeData(primarySwatch: Colors.blue),
         home: const FirstScreen());
   }
