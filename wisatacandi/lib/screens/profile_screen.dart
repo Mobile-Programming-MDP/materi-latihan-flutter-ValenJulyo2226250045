d76f4b8da27cef316a682069a4641b59adaa1c67
Ibnu-Choldun <valenjulyoarmandodavincylin_2226250045@mhs.mdp.ac.id>
2023-10-27 17:35:34 +0700

    lanjutan

--- wisatacandi/lib/screens/profile_screen.dart
+++ wisatacandi/lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisatacandi/widgets/profile_info_item.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,61 +9,136 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  //TODO: 1. Deklarasi Variable (state) yang di butuhkan
-  bool isSignIn = true;
-  String fullName = "";
-  String userName = "";
-  int favoritCandiCount = 0;
+  bool isSignedIn = true;
+  String fullName = 'Randie Sasongko';
+  String userName = 'randies';
+  int favoriteCandiCount = 2;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        alignment: Alignment.topCenter,
         children: [
           Container(
             height: 200,
             width: double.infinity,
-            color: Colors.deepPurple,
+            color: Colors.amber,
           ),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  //TODO: 2. Buat Bagian ProfileHeader yang berisi foto profil
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 200 - 50),
-                      child: Stack(
-                        alignment: Alignment.bottomRight,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.deepOrange, width: 2),
-                                shape: BoxShape.circle),
-                            child: CircleAvatar(
-                              radius: 50,
-                              backgroundImage:
-                                  AssetImage("images/placeholder_image.png"),
-                            ),
-                          ),
-                          if (isSignIn)
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.camera_alt,
-                                color: Colors.deepPurple[50],
-                              ),
-                            )
-                        ],
+          Column(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 200 - 50),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.deepPurple, width: 2),
+                            shape: BoxShape.circle),
+                        child: const CircleAvatar(
+                          radius: 50,
+                          backgroundImage:
+                              AssetImage('images/placeholder_image.png'),
+                        ),
                       ),
+                      if (isSignedIn)
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.camera_alt,
+                              color: Colors.deepPurple[50],
+                            ))
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
                     ),
-                  )
-                  //TODO: 3. Buat Bagian ProfileInfo yang berisi info profil
-                  //TODO: 4. Buat Bagian ProfileAction yang berisi TextButton Signin/ Sign out
-                ],
-              ))
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                      icon: Icons.lock,
+                      label: 'Pengguna',
+                      value: userName,
+                      iconColor: Colors.amber,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                        icon: Icons.person,
+                        label: 'Nama',
+                        value: fullName,
+                        showEditIcon: isSignedIn,
+                        onEditPressed: () {
+                          debugPrint('Icon edit ditekan ...');
+                        },
+                        iconColor: Colors.blue),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    ProfileInfoItem(
+                        icon: Icons.favorite,
+                        label: 'Favorit',
+                        value:
+                            favoriteCandiCount > 0 ? '$favoriteCandiCount' : '',
+                        iconColor: Colors.red),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Divider(
+                      color: Colors.deepPurple[100],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    isSignedIn
+                        ? TextButton(
+                            onPressed: () {},
+                            child: Text('Sign Out'),
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.amber,
+                                padding: const EdgeInsets.all(20),
+                                elevation: 5),
+                          )
+                        : TextButton(
+                            onPressed: () {},
+                            child: Text('Sign In'),
+                            style: TextButton.styleFrom(
+                                backgroundColor: Colors.amber,
+                                padding: const EdgeInsets.all(20),
+                                elevation: 5),
+                          )
+                  ],
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
